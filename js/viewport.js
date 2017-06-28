56d9c72bfba68725eb4cedbee75d73706a3dbcd3
Ishu Kalra <ishukalraish2@gmail.com>
2017-06-28 20:21:00 +0530

    keeping locus box updated

--- js/viewport.js
+++ js/viewport.js
@@ -234,7 +234,7 @@ var igv = (function (igv) {
                         newCenter = Math.round(referenceFrame.start + canvasCoords.x * referenceFrame.bpPerPixel);
                         if(referenceFrame.chrName === "all") {
                             chr = igv.browser.genome.getChromosomeCoordinate(newCenter).chr;
-                            igv.browser.search(chr);
+                            igv.browser.parseSearchInput(chr);
 
                         } else {
                             self.genomicState.referenceFrame.bpPerPixel /= 2;
@@ -326,7 +326,7 @@ var igv = (function (igv) {
                     break;
                 }
             }
-                
+
             var search =  (chromosomeNumber === 0 ? 'all' : chromosomeNumber <= 22 ? 'chr' + (chromosomeNumber).toString() : chromosomeNumber === 23 ? 'chrX' : 'chrY');
             igv.browser.parseSearchInput( search );
 
